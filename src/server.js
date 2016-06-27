098e5571f1fe44c267274ea5b2a24c1dd754019a
shermanng10 <sherman.ng10@gmail.com>
2016-06-27 18:06:37 -0400

    Ascii text generation for Slackbot operational on screens large enough for Ascii text to fit

--- src/server.js
+++ src/server.js
@@ -1,56 +1,66 @@
 import http from 'http'
 import url from 'url'
 import qs from 'querystring'
+import figlet from 'figlet'
 import config from '../config'
-import Giphy from './giphy'
+
 
 const server = http.createServer((req, res) => {
     let headers = req.headers
     let requestUrl = req.url
     let method = req.method
-    
-    function buildGifResponse(error, gifUrl){
+
+    function buildResponse(error, data){
         res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
+        res.setHeader('Content-Type', 'application/text')
         let responseBody = {
             headers: headers,
             method: method,
             url: requestUrl,
-            data: gifUrl
+            data: data
         }
-        res.write(JSON.stringify(responseBody))
+        res.write("\`\`\`" + data + "\`\`\`")
         res.end()
     }
 
-    if (method=='POST' && requestUrl == '/askgif') {
+    if (method=='POST' && requestUrl == '/getascii') {
             let reqBody = []
             req.on('error', (err) => {
                 console.error(err);
             }).on('data', (data) => {
                 reqBody.push(data)
             }).on('end', () => {
+                let textToConvert
+                let font
                 reqBody = qs.parse(Buffer.concat(reqBody).toString())
-                let gifSearchTerm = reqBody['searchTerm']
+                let bodyText = reqBody['text'].split(':')
                 let token = reqBody['token']
 
+                if (bodyText.indexOf(":")){
+                    textToConvert = bodyText[0]
+                    font = bodyText[1]
+                }
+                else{
+                    textToConvert = bodyText
+                }
+                
                 if (token != config['SLACK_TOKEN']){
                     res.statusCode = 403
                     res.write(JSON.stringify({"Info": "The token for the slash command does not match."}))
                     res.end()
                 }
-                else if (!gifSearchTerm){
+                else if (!bodyText){
                     res.statusCode = 422
-                    res.write(JSON.stringify({"Error": "Oops! You didn't enter a search term."}))
+                    res.write(JSON.stringify({"Error": "Oops! You didn't enter any text..."}))
                     res.end()
                 }
                 else {
-                    let giphy = new Giphy()
-                    giphy.getGif(gifSearchTerm, buildGifResponse)
+                    figlet.text(textToConvert, {font: font} , buildResponse)
                 }
             })
     }
     else if (method=='GET') {
-        res.write(JSON.stringify({"Message": "Hi, this is a bot to generate ASCII GIFs for Slack."}))
+        res.write(JSON.stringify({"Message": "Hi, this is a bot to generate ASCII text from text command for Slack."}))
         res.end()
     }
 })
