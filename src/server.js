6d7b9d46e1c28b21c41719f7fce210e2b5e37954
shermanng10 <sherman.ng10@gmail.com>
2016-06-29 23:32:09 -0400

    Clean up code by getting rid of res.write() and fix typo on variable name

--- src/server.js
+++ src/server.js
@@ -16,8 +16,7 @@ const server = http.createServer((req, res) => {
             "response_type": "in_channel",
             "text": "\`\`\`" + data + "\`\`\`"
         }
-        res.write(JSON.stringify(responseBody))
-        res.end()
+        res.end(JSON.stringify(responseBody))
     }
 
     if (method=='POST' && requestUrl == '/getascii') {
@@ -27,9 +26,9 @@ const server = http.createServer((req, res) => {
             }).on('data', (data) => {
                 reqBody.push(data)
             }).on('end', () => {
+                reqBody = qs.parse(Buffer.concat(reqBody).toString())
                 let textToConvert
                 let font
-                reqBody = qs.parse(Buffer.concat(reqBody).toString())
                 let bodyText = reqBody['text'].split(':')
                 let token = reqBody['token']
 
@@ -43,27 +42,23 @@ const server = http.createServer((req, res) => {
                 
                 if (token != config['SLACK_TOKEN']){
                     res.statusCode = 403
-                    res.write(JSON.stringify({"Info": "The token for the slash command does not match."}))
-                    res.end()
+                    res.end(JSON.stringify({"Info": "The token for the slash command does not match."}))
                 }
                 else if (!bodyText){
                     res.statusCode = 422
-                    res.write(JSON.stringify({"Error": "Oops! You didn't enter any text..."}))
-                    res.end()
+                    res.end(JSON.stringify({"Error": "Oops! You didn't enter any text..."}))
                 }
                 else {
                     figlet.text(textToConvert, {font: font} , buildResponse)
                 }
             })
     }
-    else if (method=='GET' && requestURL == '/') {
-        res.write(JSON.stringify({"Message": "Hi, this is a bot to generate ASCII text from text command for Slack."}))
-        res.end()
+    else if (method=='GET' && requestUrl == '/') {
+        res.end(JSON.stringify({"Message": "Hi, this is a bot to generate ASCII text from text command for Slack."}))
     }
     else{
         res.statusCode = 404
-        res.write(JSON.stringify({"Message": "Resource not found."}))
-        res.end()
+        res.end(JSON.stringify({"Message": "Resource not found."}))
     }
 })
 
